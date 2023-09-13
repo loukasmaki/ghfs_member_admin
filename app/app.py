0288330773e18a4bdede8989876cac58a6cf473b
Petri Loukasmäki <petri.loukasmaki@gmail.com>
2023-09-13 20:30:42 +0200

    First upload. Currently working on forms

---
+++ app/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+# Secret key expected by wtforms REMEMBER TO CHANGE WHEN DEPLOYING ALSO WILL BE UPDATED WITH A SAFER APPROACH LATER
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+
+#class NameForm
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+# Routes
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data=''
+    user_agent = request.headers.get('User-Agent')
+    return render_template('index.html', user_agent=user_agent, current_time=datetime.utcnow(), name=name, form=form)
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+# Errorhandler
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
